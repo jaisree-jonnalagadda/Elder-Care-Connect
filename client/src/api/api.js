baf4979424d5f0e64269b483da7e06e43b0e933b
Jai Sree Renuka Jonnalagadda <jaisree0605@gmail.com>
2026-07-29 00:35:57 +0530

    Connected frontend with deployed backend

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-    baseURL:"http://localhost:5000/api"
-
+  baseURL: "https://elder-care-connect-backend.onrender.com/api",
 });
-
 
 export default API;
